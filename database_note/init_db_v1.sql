83be20b04991bc574a7def6ba10b809ef7b41e46
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2025-10-24 14:29:14 +0700

    db fin

--- database_note/init_db_v1.sql
+++ database_note/init_db_v1.sql
@@ -1,8 +1,9 @@
--- Drop tables in reverse order to avoid foreign key conflicts
-DROP TABLE IF EXISTS mac_text
+DROP TABLE IF EXISTS mac_text;
 
--- Type Layer: Reference data
 CREATE TABLE mac_text (
     id SERIAL PRIMARY KEY,
-    description VARCHAR(100) NOT NULL
+    mac_address VARCHAR(17) NOT NULL UNIQUE,
+    description TEXT NOT NULL
 );
+
+CREATE INDEX idx_mac_text_mac ON mac_text(mac_address);
